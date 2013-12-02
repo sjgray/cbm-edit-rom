d50ba9dd85efdba039bc02e2b937da382610f114
Steve Gray <sjgray@rogers.com>
2013-12-02 18:59:27 -0500

    adding includes for "old" crtc tables

--- editrom.asm
+++ editrom.asm
@@ -1183,7 +1183,23 @@ SOUND_TAB
 
 ;************* CRTC Chip Register Setup Tables (OLD)
 
-		!source "crtc-80-50hz-old.asm"
+!if COLUMNS = 80 {
+	!if REFRESH = 0 { !source "crtc-80-50hz-old.asm" }
+	!if REFRESH = 1 { !source "crtc-80-60hz-old.asm" }
+}
+
+!if COLUMNS = 40 {
+	!if SOFT40 = 1 {
+		!if REFRESH = 0 { !source "crtc-soft40-50hz-old.asm" }
+		!if REFRESH = 1 { !source "crtc-soft40-60hz-old.asm" }
+	} ELSE {
+		!if REFRESH = 0 { !source "crtc-40-50hz-old.asm" }
+		!if REFRESH = 1 { !source "crtc-40-60hz-old.asm" }
+		!if REFRESH = 2 { !source "crtc-40-pal-old.asm" }
+		!if REFRESH = 3 { !source "crtc-40-ntsc-old.asm" }
+	}
+}
+
 
 		!byte $28			; ??????
            !fill $e7ff-*,$ff	; 83 bytes - Fill to end of 2K 
