0ab7a8a74fe85880c4935635d3f8c05333c67129
Steve Gray <sjgray@rogers.com>
2013-12-02 21:01:32 -0500

    adding descriptions to std zp and low-mem locations

--- stdlowmem.asm
+++ stdlowmem.asm
@@ -1,22 +1,22 @@
 ; PET/CBM EDIT ROM - Standard Low Mem locations/variables/storage
 ; ================
 
-STACK = $0100
-BUF = $0200
-LAT = $0251
-FAT = $025b
-SAT = $0265
-KEYD = $026f
-TAPEB1 = $027a
-NextTab = $033a
-DOS_Drive_1 = $033b
-DOS_Drive_2 = $033c
-DOS_Length = $033d
-DOS_Syntax = $033e
-DOS_Id = $033f
-DOS_Command_Length = $0341
-DOS_Filename = $0342
-DOS_Command_Buffer = $0353
-TABS_SET = $03ee
-Reset_Vector = $03fa
-Reset_Param = $03fc
+STACK 			= $0100	;Processor Stack
+BUF 				= $0200	;MLM work area; Input buffer
+LAT 				= $0251	;File Logical Address Table
+FAT 				= $025b	;File Device Number Table
+SAT 				= $0265	;File Secondary Address Table
+KEYD 				= $026f	;Keyboard input buffer
+TAPEB1 			= $027a	;TAPE#1 input buffer
+NextTab 			= $033a	;DOS character pointer
+DOS_Drive_1 		= $033b	;DOS drive 1 flag
+DOS_Drive_2 		= $033c	;DOS drive 2 flag
+DOS_Length 		= $033d	;DOS length/write flag
+DOS_Syntax 		= $033e	;DOS syntax flags
+DOS_Id 			= $033f	;DOS disk ID
+DOS_Command_Length 	= $0341	;DOS command string count
+DOS_Filename 		= $0342	;DOS filename buffer
+DOS_Command_Buffer 	= $0353	;DOS command string buffer
+TABS_SET 			= $03ee	;80 column TAB stop table
+Reset_Vector 		= $03fa	;Monitor extension vector
+Reset_Param 		= $03fc	;IEEE timeout defeat; $FF=disable
