1f97a62c1da7cc655648dc0befdff6f08e73a368
Steve Gray <sjgray@rogers.com>
2013-12-11 14:35:04 -0500

    added binary file compare (change path if needed)

--- make.bat
+++ make.bat
@@ -1,2 +1,4 @@
 @rem pet/cbm edit roms - assembler directives are at the top of "edit.asm"... change as needed
 acme edit.asm 
+@rem --- now compare result to a target rom
+FC >c:\temp\colorpet\out.txt /b editrom.bin c:\temp\colorpet\edit-4-80-b-50hz-901474-04.bin
