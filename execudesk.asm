61020f42aadb3a4c0309688e1756e881018bacb9
Steve Gray <sjgray@rogers.com>
2014-01-24 10:44:22 -0500

    More Execudesk formatting

--- execudesk.asm
+++ execudesk.asm
@@ -19,164 +19,172 @@ $E900: 4C 9E EC  JMP EDESK_IRQ_HANDLER		; L_EC9E
 ;--------------- Execudesk Menu Strings
 ; NULL-terminated strings
 
-MENUSTRING1
-$E903:           !BYTE 93,12,20,20,20,20,20,20 ;execudesk menu
-$E90B:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$E913:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$E91B:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$E923:           !BYTE 20,20,C5,D8,C5,C3,D5,20 ;execudesk menu
-$E92B:           !BYTE 2D,20,C4,C5,D3,CB,20,20 ;execudesk menu
-$E933:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$E93B:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$E943:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$E94B:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$E953:           !BYTE 20,0D,0D,0D,20,20,20,20 ;execudesk menu
-$E95B:           !BYTE 20,20,20,20,20,20,20,12 ;execudesk menu
-$E963:           !BYTE 20,31,20,92,20,20,20,20 ;execudesk menu
-$E96B:           !BYTE 20,20,D7,4F,52,44,20,D0 ;execudesk menu
-$E973:           !BYTE 52,4F,43,45,53,53,4F,52 ;execudesk menu
-$E97B:           !BYTE 0D,0D,20,20,20,20,20,20 ;execudesk menu
-$E983:           !BYTE 20,20,20,20,20,12,20,32 ;execudesk menu
-$E98B:           !BYTE 20,92,20,20,20,20,20,20 ;execudesk menu
-$E993:           !BYTE D3,50,52,45,41,44,20,D3 ;execudesk menu
-$E99B:           !BYTE 48,45,45,54,0D,0D,20,20 ;execudesk menu
-$E9A3:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$E9AB:           !BYTE 20,12,20,33,20,92,20,20 ;execudesk menu
-$E9B3:           !BYTE 20,20,20,20,C4,41,54,41 ;execudesk menu
-$E9BB:           !BYTE 20,C2,41,53,45,20,CD,41 ;execudesk menu
-$E9C3:           !BYTE 4E,41,47,45,52,0D,0D,00 ;execudesk menu
+EDSTRING1
+$E903:          !BYTE 93,12			; <CLS><RVS>
+		!TEXT "                                "
+		!BYTE C5,D8,C5,C3,D5,20		; <SHIFT>"EXECU 
+		!BYTE 2D,20,C4,C5,D3,CB		; - DESK"
+		!TEXT "                                   "
+		!BYTE 0D,0D,0D			; <CR>
+		!BYTE "           "
+		!BYTE 12,20,31,20,92		; <RVS> 1 <RVSOFF>
+		!BYTE "      "
+$E96B:          !BYTE D7			; SHIFT-W
+		!TEXT "ORD "
+		!BYTE D0			; SHIFT-P
+$E973:          !BYTE "ROCESSOR"
+$E97B:          !BYTE 0D,0D
+		!TEXT "           "
+		!BYTE 12,20,32,20,92		; <RVS> 2 <RVSOFF>
+		!TEXT "      "
+$E993:          !BYTE D3			; SHIFT-S
+		!TEXT "PREAD "
+		!BYTE D3			; SHIFT-S
+$E99B:          !TEXT "HEET"
+		!BYTE 0D,0D
+		!TEXT "           "
+		!BYTE 12,20,33,20,92		; <RVS> 3 <RVSOFF>
+		!TEXT "      "
+		!BYTE C4			; SHIFT-D
+		!TEXT "ATA "
+		!BYTE C2			; SHIFT-B
+		!TEXT "ASE "
+		!BYTE CD
+		!TEXT "ANAGER
+		!BYTE 0D,0D
+		!BYTE 0
 
 ;--------------- STRING
 
-MENUSTRING2
-$E9CB:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$E9D3:           !BYTE 20,20,20,12,20,34,20,92 ;execudesk menu
-$E9DB:           !BYTE 20,20,20,20,20,20,C6,49 ;execudesk menu
-$E9E3:           !BYTE 4E,41,4E,43,49,41,4C,20 ;execudesk menu
-$E9EB:           !BYTE C3,41,4C,43,55,4C,41,54 ;execudesk menu
-$E9F3:           !BYTE 49,4F,4E,53,0D,0D,20,20 ;execudesk menu
-$E9FB:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$EA03:           !BYTE 20,12,20,35,20,92,20,20 ;execudesk menu
-$EA0B:           !BYTE 20,20,20,20,D4,45,52,4D ;execudesk menu
-$EA13:           !BYTE 49,4E,41,4C,20,C3,4F,4D ;execudesk menu
-$EA1B:           !BYTE 4D,55,4E,49,43,41,54,49 ;execudesk menu
-$EA23:           !BYTE 4F,4E,53,0D,0D,20,20,20 ;execudesk menu
-$EA2B:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$EA33:           !BYTE 12,20,36,20,92,20,20,20 ;execudesk menu
-$EA3B:           !BYTE 20,20,20,D3,59,53,54,45 ;execudesk menu
-$EA43:           !BYTE 4D,20,D5,54,49,4C,49,54 ;execudesk menu
-$EA4B:           !BYTE 49,45,53,0D,0D,00
+EDSTRING2
+$E9CB:          !TEXT "           "
+$E9D3:          !BYTE 12,20,34,20,92		; <RVS> 4 <RVSOFF>
+$E9DB:          !BYTE "      "
+		!BYTE C6			; SHIFT-F
+		!TEXT "INANCIAL "
+$E9EB:          !BYTE C3			; SHIFT-C
+		!TEXT "ALCULATIONS"
+		!BYTE 0D,0D
+		!TEXT "           "
+$EA03:          !BYTE 12,20,35,20,92		; <RVS> 5 <RVSOFF>
+		!BYTE "      "
+		!BYTE D4			; SHIFT-T
+		!TEXT "ERMINAL "
+		!BYTE C3			; SHIFT-C
+		!TEXT "OMMUNICATIONS"
+		!BYTE 0D,0D
+		!TEXT "           "
+$EA33:          !BYTE 12,20,36,20,92		; <RVS> 6 <RVSOFF>
+		!TEXT "      "
+$EA3B:          !BYTE D3			; SHIFT-S
+		!TEXT "YSTEM "
+		!BYTE D5
+		!TEXT "TILITIES"
+		!BYTE 0D,0D
+		!BYTE 0
 
 ;--------------- STRING
 
-MENUSTRING3
-$EA51:		 !BYTE 20,20 		      ;execudesk menu
-$EA53:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$EA5B:           !BYTE 20,12,20,37,20,92,20,20 ;execudesk menu
-$EA63:           !BYTE 20,20,20,20,D3,59,53,54 ;execudesk menu
-$EA6B:           !BYTE 45,4D,20,D3,48,55,54,20 ;execudesk menu
-$EA73:           !BYTE C4,4F,57,4E,0D,0D,20,20 ;execudesk menu
-$EA7B:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$EA83:           !BYTE 20,12,20,38,20,92,20,20 ;execudesk menu
-$EA8B:           !BYTE 20,20,20,20,D5,53,45,52 ;execudesk menu
-$EA93:           !BYTE 20,D3,4F,46,54,57,41,52 ;execudesk menu
-$EA9B:           !BYTE 45,0D,0D,20,20,20,20,20 ;execudesk menu
-$EAA3:           !BYTE 20,20,20,20,20,20,12,20 ;execudesk menu
-$EAAB:           !BYTE 39,20,92,20,20,20,20,20 ;execudesk menu
-$EAB3:           !BYTE 20,C5,58,49,54,20,54,4F ;execudesk menu
-$EABB:           !BYTE 20,C2,41,53,49,43,0D,0D ;execudesk menu
-$EAC3:           !BYTE 00                      ;execudesk menu
+EDSTRING3
+$EA51:		!TEXT "           "
+$EA5B:          !BYTE 12,20,37,20,92		; <RVS> 7 <RVSOFF>
+		!TEXT "      "
+		!BYTE D3			; SHIFT-S
+		!TEXT "YSTEM "
+		!BYTE D3			; SHIFT-S
+		!TEXT "HUT "
+$EA73:          !BYTE C4
+		!TEXT "DOWN"
+		!BYTE 0D,0D
+		!TEXT "           "
+		!BYTE 12,20,38,20,92		; <RVS> 8 <RVSOFF>
+		!TEXT "      "
+           	!BYTE D5			; SHIFT-U
+		!TEXT "SER "
+		!BYTE D3			; SHIFT-S
+		!TEXT "OFTWARE"
+		!BYTE 0D,0D
+		!TEXT "           "	
+		!BYTE 12,20,39,20,92		; <RVS> 9 <RVSOFF>
+		!TEXT "      "
+		!BYTE C5			; SHIFT-E
+		!TEXT "XIT TO "
+		!BYTE C2
+		!TEXT "ASIC"
+		!BYTE 0D,0D
+$EAC3:          !BYTE 0
 
 ;--------------- STRING
 
-MENUSTRING4
-$EAC4:		 !BYTE 0D,12,20,20,20,20,20    ;execudesk menu
-$EACB:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$EAD3:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$EADB:           !BYTE 20,20,20,20,20,D0,52,45 ;execudesk menu
-$EAE3:           !BYTE 53,53,20,D3,45,4C,45,43 ;execudesk menu
-$EAEB:           !BYTE 54,49,4F,4E,20,C1,42,4F ;execudesk menu
-$EAF3:           !BYTE 56,45,20,20,20,20,20,20 ;execudesk menu
-$EAFB:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$EB03:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$EB0B:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$EB13:           !BYTE 20,92,00
+EDSTRING4
+$EAC4:		!BYTE 0D,12
+		!TEXT "                          "
+		!BYTE D0			; SHIFT-P
+		!TEXT "RESS "
+		!BYTE D3			; SHIFT-S
+		!TEXT "ELECTION "
+		!BYTE C1			; SHIFT-A
+		!TEXT "BOVE                               "
+		!BYTE 92			; <RVSOFF>
+		!BYTE 0
 
 ;--------------- STRING
 
-MENUSTRING5
-$EB16:		 !BYTE 93,11,11,11,11 ;execudesk menu
-$EB1B:           !BYTE 11,11,11,11,11,11,20,20 ;execudesk menu
-$EB23:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$EB2B:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$EB33:           !BYTE 20,20,20,20,12,20,D0,52 ;execudesk menu
-$EB3B:           !BYTE 4F,47,52,41,4D,20,4E,4F ;execudesk menu
-$EB43:           !BYTE 54,20,46,4F,55,4E,44,20 ;execudesk menu
-$EB4B:           !BYTE 4F,4E,20,44,49,53,4B,20 ;execudesk menu
-$EB53:           !BYTE 0D,00
+EDSTRING5
+$EB16:		!BYTE 93,11,11,11,11
+$EB1B:          !BYTE 11,11,11,11,11,11
+		!TEXT "                      "
+		!BYTE 12,20,D0			; <RVS> SHIFT-P
+		!TEXT "ROGRAM NOT FOUND ON DISK "
+$EB53:          !BYTE 0D
+		!BYTE 0
 
 ;--------------- STRING
 
-MENUSTRING6
-$EB55:		 !BYTE 11,20,20,20,20,20       ;execudesk menu
-$EB5B:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$EB63:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$EB6B:           !BYTE 20,12,20,D3,54,52,49,4B ;execudesk menu
-$EB73:           !BYTE 45,20,41,4E,59,20,4B,45 ;execudesk menu
-$EB7B:           !BYTE 59,20,54,4F,20,54,52,59 ;execudesk menu
-$EB83:           !BYTE 20,41,47,41,49,4E,20,0D ;execudesk menu
-$EB8B:           !BYTE 00                      ;execudesk menu
+EDSTRING6
+$EB55:		!BYTE 11
+		!TEXT "                      "
+		!BYTE 12,20,D3			; <RVS> SHIFT-S
+		!TEXT "TRIKE ANY KEY TO TRY AGAIN "
+		!BYTE 0D
+$EB8B:          !BYTE 0
 
 ;--------------- STRING
 
-MENUSTRING7
-$EB8C:		 !BYTE 93,11,11,11,11,11,11    ;execudesk menu
-$EB93:           !BYTE 11,11,11,11,20,20,20,20 ;execudesk menu
-$EB9B:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$EBA3:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$EBAB:           !BYTE 20,20,12,20,C4,49,53,4B ;execudesk menu
-$EBB3:           !BYTE 20,44,52,49,56,45,20,4E ;execudesk menu
-$EBBB:           !BYTE 4F,54,20,43,4F,4E,4E,45 ;execudesk menu
-$EBC3:           !BYTE 43,54,45,44,20,0D,00
+EDSTRING7
+$EB8C:		!BYTE 93				; <CLS>
+		!BYTE 11,11,11,11,11,11,11,11,11,11	; <DOWN>
+		!TEXT "                      "		; SPACES
+	        !BYTE 12,20,C4				; <RVS> SHIFT-D
+		!TEXT "ISK DRIVE NOT CONNECTED "
+		!BYTE 0D,00
 
 
-;--------------- Menu Strings
-; In table
+;--------------- Filename Strings
 
-MSTRING1
-$EBCA:		 !BYTE 30                      ;execudesk menu
-$EBCB:           !BYTE 3A,57,4F,52,44,50,52,4F ;execudesk menu
-$EBD3:           !BYTE 43,45,53,53,4F,52,2C,50 ;execudesk menu
-$EBDB:           !BYTE 00
+EDTAB1		!TEXT "0:WORDPROCESSOR,P"
+		!BYTE 0
 
-MSTRING2
-$EBDC:		 !BYTE 30,3A,53,50,52,45,41,44 
-$EBE3:           !BYTE 53,48,45,45,54,2C,50,00
+EDTAB2  	!TEXT "0:SPREADSHEET,P"
+		!BYTE 0
 
-MSTRING3
-$EBEC:		 !BYTE 30,3A,44,41,54,41,42,41,53,45,2C,50,00
+EDTAB3		!TEXT "0:DATABASE,P"
+		!BYTE 0
 
-MSTRING4
-$EBF9:		 !BYTE 30,3A 			;execudesk menu
-$EBFB:           !BYTE 46,49,4E,41,4E,43,45,2C 	;execudesk menu
-$EC03:           !BYTE 50,00
+EDTAB4		!TEXT "0:FINANCE,P"
+        	!BYTE 0
 
-MSTRING5
-$EC05:		 !BYTE 30,3A,54,45,52,4D 	;execudesk menu
-$EC0B:           !BYTE 49,4E,41,4C,2C,50,00
+EDTAB5		!TEXT "0:TERMINAL,P"
+		!BYTE 0
 
-MSTRING6
-$EC12:           !BYTE 30 ;execudesk menu
-$EC13:           !BYTE 3A,55,54,49,4C,49,54,49 ;execudesk menu
-$EC1B:           !BYTE 45,53,2C,50,00
+EDTAB6		!TEXT "0:UTILITIES,P"
+		!BYTE 0
 
-MSTRING7
-$EC20		 !BYTE 30,3A,53 ;execudesk menu
-$EC23:           !BYTE 48,55,54,44,4F,57,4E,2C ;execudesk menu
-$EC2B:           !BYTE 50,00
+EDTAB7		!TEXT "0:SHUTDOWN,P"
+		!BYTE 0
 
-MSTRING8
-$EC2D:		 !BYTE 30,3A,55,53,45,52,2C,50,00
+EDTAB8		!TEXT "0:USER,P"
+		!BYTE 0
 
 ;--------------- String
 
@@ -186,28 +194,28 @@ $EC36:		 !BYTE 11,0F,0C,0B,0C,0D,0C,08
 
 EDMTABLE
 $EC3E:
-	!WORD MSTRING1		; BYT CA,EB	; String 1 at $EBCA
-	!WORD MSTRING2		; BYT DC,EB	; String 2 at $EBDC
-	!WORD MSTRING3		; BYT EC,EB	; String 3 at $EBEC
-	!WORD MSTRING4		; BYT F9,EB	; String 4 at $EBF9
-	!WORD MSTRING5		; BYT 05,EC	; String 5 at $EC05
-	!WORD MSTRING6		; BYT 12,EC	; String 6 at $EC12
-	!WORD MSTRING7		; BYT 20,EC	; String 7 at $EC20
-	!WORD MSTRING8		; BYT 2D,EC	; String 8 at $EC2D
+		!WORD EDTAB1		; String 1 at $EBCA
+		!WORD EDTAB2		; String 2 at $EBDC
+		!WORD EDTAB3		; String 3 at $EBEC
+		!WORD EDTAB4		; String 4 at $EBF9
+		!WORD EDTAB5		; String 5 at $EC05
+		!WORD EDTAB6		; String 6 at $EC12
+		!WORD EDTAB7		; String 7 at $EC20
+		!WORD EDTAB8		; String 8 at $EC2D
 
-;--------------- 
+;--------------- ?????
 
-$EC4E:		 !BYTE 52,55,4E,0D,00          ;execudesk menu
-$EC53:           !BYTE 93,0D,0D,0D,0D,0D,0D,0D ;execudesk menu
-$EC5B:           !BYTE 0D,0D,0D,0D,20,20,20,20 ;execudesk menu
-$EC63:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$EC6B:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$EC73:           !BYTE 20,20,20,20,20,20,20,20 ;execudesk menu
-$EC7B:           !BYTE 20,20,20,CF,CE,C5,20,CD ;execudesk menu
-$EC83:           !BYTE CF,CD,C5,CE,D4,20,D0,CC ;execudesk menu
-$EC8B:           !BYTE C5,C1,D3,C5,20,2E,2E,2E ;execudesk menu
-$EC93:           !BYTE 0D,0D,0D,0D,0D,0D,0D,0D ;execudesk menu
-$EC9B:           !BYTE 0D,0D,00                ;execudesk menu
+$EC4E:		!TEXT "RUN"
+		!BYTE 0D,00
+$EC53:          !BYTE 93,0D,0D,0D,0D,0D,0D,0D
+$EC5B:          !BYTE 0D,0D,0D,0D
+		!TEXT "                               "
+		!BYTE CF,CE,C5,20,CD
+$EC83:          !BYTE CF,CD,C5,CE,D4,20,D0,CC
+$EC8B:          !BYTE C5,C1,D3,C5,20,2E,2E,2E
+$EC93:          !BYTE 0D,0D,0D,0D,0D,0D,0D,0D
+$EC9B:          !BYTE 0D,0D
+		!BYTE 0
 
 ;--------------- New IRQ Handler entry point. 
 ;
@@ -221,7 +229,7 @@ $EC9F: AD 00 80  LDA SCREEN 				; start of screen ram
 $ECA2: C9 2A     CMP #$2A				; Is it "*"?
 $ECA4: F0 04     BEQ L_ECAA				; Yes, restore IRQ
 $ECA6: 68        PLA					; Restore A
-$ECA7: 4C 55 E4  JMP  L_E455				; normal IRQ
+$ECA7: 4C 55 E4  JMP L_E455				; normal IRQ
 
 ;--------------- Prep for Execudesk
 ;
@@ -249,27 +257,27 @@ $ECC2: 48        PHA
 $ECC3: 48        PHA
 $ECC4: 48        PHA
 $ECC5: 48        PHA
-$ECC6: 4C 55 E4  JMP  L_E455				; Do regular IRQ
+$ECC6: 4C 55 E4  JMP L_E455				; Do regular IRQ
 
 ;--------------- Execudesk Menu System
 
 EDESKMENU
 
 L_ECC9:
-$ECC9: A9 03     LDA #<MENUSTRING1			; $E903
-$ECCB: A0 E9     LDY #>MENUSTRING1
+$ECC9: A9 03     LDA #<EDSTRING1			; $E903
+$ECCB: A0 E9     LDY #>EDSTRING1
 $ECCD: 20 1D BB  JSR $BB1D				; Print a string
 
-$ECD0: A9 CB     LDA #<MENUSTRING2			; $E9CB
-$ECD2: A0 E9     LDY #>MENUSTRING2
+$ECD0: A9 CB     LDA #<EDSTRING2			; $E9CB
+$ECD2: A0 E9     LDY #>EDSTRING2
 $ECD4: 20 1D BB  JSR $BB1D				; Print a string
 
-$ECD7: A9 51     LDA #<MENUSTRING3			; $EA51
-$ECD9: A0 EA     LDY #>MENUSTRING3
+$ECD7: A9 51     LDA #<EDSTRING3			; $EA51
+$ECD9: A0 EA     LDY #>EDSTRING3
 $ECDB: 20 1D BB  JSR $BB1D				; Print a string
 
-$ECDE: A9 C4     LDA #<MENUSTRING4			; $EAC4
-$ECE0: A0 EA     LDY #>MENUSTRING4
+$ECDE: A9 C4     LDA #<EDSTRING4			; $EAC4
+$ECE0: A0 EA     LDY #>EDSTRING4
 $ECE2: 20 1D BB  JSR $BB1D				; Print a string
 
 ;--------------- Get keys and wait for valid selection
@@ -326,25 +334,25 @@ $ED2F: C9 00     CMP #$00
 $ED31: F0 4A     BEQ L_ED7D
 $ED33: 48        PHA
 $ED34: A9 01     LDA #$01
-$ED36: 20 E2 F2  JSR  $F2E2
+$ED36: 20 E2 F2  JSR $F2E2
 $ED39: 68        PLA
 $ED3A: C9 74     CMP #$74
 $ED3C: D0 0A     BNE L_ED48
 
 L_ED3E:
-$ED3E: A9 8C     LDA #<MENUSTRING6				; $EB8C
-$ED40: A0 EB     LDY #>MENUSTRING6
+$ED3E: A9 8C     LDA #<EDSTRING7				; $EB8C
+$ED40: A0 EB     LDY #>EDSTRING7
 $ED42: 20 1D BB  JSR $BB1D					; Print a string
 $ED45: 4C 4F ED  JMP L_ED4F
 
 L_ED48:
-$ED48: A9 16     LDA #<MENUSTRING7				; $EB16
-$ED4A: A0 EB     LDY #>MENUSTRING7
+$ED48: A9 16     LDA #<EDSTRING5				; $EB16
+$ED4A: A0 EB     LDY #>EDSTRING5
 $ED4C: 20 1D BB  JSR $BB1D					; Print a string
 
 L_ED4F:
-$ED4F: A9 55     LDA #<MENUSTRING8				; $EB55
-$ED51: A0 EB     LDY #>MENUSTRING8
+$ED4F: A9 55     LDA #<EDSTRING6				; $EB55
+$ED51: A0 EB     LDY #>EDSTRING6
 $ED53: 20 1D BB  JSR $BB1D					; Print a string
 
 L_ED56:
