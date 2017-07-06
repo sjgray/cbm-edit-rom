17ac5cfe17e49fc05b48845327dbf8b965e8345a
Steve Gray <sjgray@rogers.com>
2017-07-06 08:42:50 -0400

    Fix CRTC settings for 9" Monitor
    Eliminate need for inverter chip by changing register 12 to 0.

--- crtc-4080-9inch.asm
+++ crtc-4080-9inch.asm
@@ -3,18 +3,19 @@
 ;
 ; These CRTC values were supplied by Toni Ellis. They are for use with the small 9" CRTC monitor.
 ;
-; These tables are used to program the CRTC chip to set the screen to the proper rows, columns, and timing 
+; These tables are used to program the CRTC chip to set the screen to the proper rows, columns, and timing.
+; NOTE: for 9" Monitors the video is NOT inverted like the 12"! 
 
 ;--------- Text Mode - 40/80 Column, 50 Hz, 15 kHz
 
 CRT_CONFIG_TEXT
     		!byte $3F,$28,$30,$8E,$20,$00,$19,$1C
-    		!byte $00,$09,$00,$00,$10,$00,$00,$00
+    		!byte $00,$09,$00,$00,$00,$00,$00,$00
     		!byte $00,$00 
 
 ;--------- Graphics Mode - 40/80 Column, 50 Hz, 15 kHz
 
 CRT_CONFIG_GRAPHICS
     		!byte $3F,$28,$30,$8E,$20,$00,$19,$1C
-    		!byte $00,$07,$00,$00,$10,$00,$00,$00
+    		!byte $00,$07,$00,$00,$00,$00,$00,$00
     		!byte $00,$00 
