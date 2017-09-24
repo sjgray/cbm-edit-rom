89eefe7bc2ce409b98a7e4fa4bf9f35590aeb6d3
Steve Gray <sjgray@rogers.com>
2017-09-24 15:53:35 -0400

    Soft40 for PAL fix
    Thanks to Dave Curran for a working config!

--- crtc-soft40-pal.asm
+++ crtc-soft40-pal.asm
@@ -5,17 +5,20 @@
 ; Soft40 = Hardware setup for 80 column, but only showing 40 columns by increasing LEFT/RIGHT margins
 ;          Similar to how the "80240.PRG" software does it.
 
-;--------- Text Mode - Soft40, 50 Hz, 20 kHz
-; TODO: FIX TABLES!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+;--------- Text Mode - Soft40, 50 Hz, PAL 15.7 kHz
 
 CRT_CONFIG_TEXT
-    		!byte $31,$14,$1f,$0f,$28,$05,$19,$21
+;    		!byte $31,$14,$1f,$0f,$28,$05,$19,$21
+    		!byte $3f,$14,$28,$14,$24,$7b,$19,$20	
     		!byte $00,$09,$00,$00,$10,$00,$00,$00
     		!byte $00,$00 
 
-;--------- Graphics Mode - Soft40, 50 Hz, 20 kHz
+;--------- Graphics Mode - Soft40, 50 Hz, PAL 15.7 kHz
 
 CRT_CONFIG_GRAPHICS
-    		!byte $31,$14,$1f,$0f,$28,$05,$19,$21
+ ;   		!byte $31,$14,$1f,$0f,$28,$05,$19,$21
+    		!byte $3f,$14,$28,$14,$24,$7b,$19,$20
     		!byte $00,$07,$00,$00,$10,$00,$00,$00
     		!byte $00,$00
+
+; Thanks to Dave Curran for the providing the above fix!
