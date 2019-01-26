e7aa0664e77b23b7f27376399d1648438f4482a0
Steve Gray <sjgray@rogers.com>
2019-01-26 11:22:40 -0500

    Build sjg-edit-80-b-50-esc-wedge-ss80-reboot-backarrow (2019-01-26).bin

--- edit.asm
+++ edit.asm
@@ -26,30 +26,30 @@
 ; The following DATE and COMMENT strings will be placed in the IO area (if used).
 ; Take care that comments do not overflow into code space!
 
-!macro DATE    { !pet "2018-12-30" }
-!macro COMMENT { !pet "sjg-edit-c1-b-60-optrom2-esc-wedge-execudesk-reboot-backarrow (2018-12-30)" }
+!macro DATE    { !pet "2019-01-26" }
+!macro COMMENT { !pet "sjg-edit-80-b-50-esc-wedge-ss80-reboot-backarrow (2019-01-26)" }
 
 ;----------------------------------------------------------------------------------------------------------------------------
 
 ; VARIABLE	  FEATURE			VALID OPTIONS			NOTES / FUTURE OPTIONS
 ;---------	  -------			-------------			----------------------
 CODEBASE  = 1   ; Code Base			0=4000, 1=8000, 2=8296		
-OPTROM    = 1   ; Location of EXT code		0=Ext Edit ($E800-EFFF), 1=$9000, 2=$A000 (note: code could also extend past end of 1)
+OPTROM    = 0   ; Location of EXT code		0=Ext Edit ($E800-EFFF), 1=$9000, 2=$A000 (note: code could also extend past end of 1)
 
 KEYSCAN   = 1   ; Keyboard Scanner		0=Graphic,1=Business,2=DIN
 KEYBOARD  = 1	; Keyboard type:		0=N-QWERTY,1=B-QWERTY,2=DIN,3=C64,4=B-SJG,5=N-SJG,6=B-QWERTZ,7=B-AZERTY,8=CBM-II (req hw mod)
-REFRESH   = 1	; Screen refresh:		0=Euro,1=N.America,2=PAL,3=NTSC,4=9",5=8296/35-line,6=8296/32-line,7=8296D Euro,8=8296D N.A.
+REFRESH   = 0	; Screen refresh:		0=Euro,1=N.America,2=PAL,3=NTSC,4=9",5=8296/35-line,6=8296/32-line,7=8296D Euro,8=8296D N.A.
 REPEATOPT = 0	; Key Repeat Option		0=No (Always ON), 1=Yes
 COLUMNS   = 80	; Screen Width			40,80,90,32 columns		Special cases 32 or 90.
 ROWS      = 25  ; Screen Height			25,35,16 rows			Special cases 16 or 35.
-HERTZ     = 60	; Line Frequency (Clock):	50=Euro,60=N.America
+HERTZ     = 50	; Line Frequency (Clock):	50=Euro,60=N.America
 IRQFIX    = 1   ; Fix Jiffy Clock		0=No, 1=Yes			Still needs investigating.
 BOOTCASE  = 0	; Initial Screen Mode		0=Text/Lower, 1=Upper/Graphics
 
 ESCCODES  = 1	; Add ESC codes? 		0=No, 1=Yes			Enable when using COLOURPET or SS40.
 AUTORUN   = 1   ; Set for BANNER and/or WEDGE	0=No, 1=Yes			Enable if you use EITHER banner and/or wedge.
 BYPASS    = 1   ; Check for key to bypass 	0=No, 1=Yes			Hold key on ROW9 to bypass.
-BANNER    = 16  ; Custom Banner (power on msg)  0=No, N=Banner# (1-12, or 99)   Valid when AUTORUN=1. Refer to docs or source. 99=debug message
+BANNER    = 16  ; Custom Banner (power on msg)  0=No, N=Banner# (1-16, or 99)   Valid when AUTORUN=1. Refer to docs or source. 99=debug message
 WEDGE     = 1	; DOS Wedge			0=No, 1=Yes			Valid when AUTORUN=1.
 WEDGEMSG  = 0	; Show wedge message?		0=No, 1=Yes
 SOFT40    = 0	; 40 columns on 8032s?		0=No, 1=Yes			Do NOT enable SOFT40 and SS40 at the same time!
@@ -68,11 +68,11 @@ BYPASSBG  = 0   ; ColourPET Bypass BG     	0 to 15 RGBI
 
 MOT6845   = 0   ; Is CRTC a Motorola6845?       0=No, 1=Yes			Probably 0=No for compatibility.
 REBOOT    = 1	; Add keyboard reboot? 		0=No, 1=Yes
-EXECUDESK = 2	; Add Execudesk Menu?		0=No, 1=Yes, 2=Yes/OPTROM>0	Note: Requires BOOT to TEXT mode!
+EXECUDESK = 0	; Add Execudesk Menu?		0=No, 1=Yes, 2=Yes/OPTROM>0	Note: Requires BOOT to TEXT mode!
 SILENT    = 0	; Disable BELL/CHIME		0=Normal, 1=Disabled
 CRUNCH    = 0   ; Remove unneeded code?		0=No, 1=Yes			Removes NOPs, filler, and unreachable code.
 BACKARROW = 1   ; Patch for screen mode toggle  0=NO, 1=Yes 2K, 2=Yes EXT	Note: B keyboard scanner only.
-INFO      = 2   ; Add project info to code area 0=NO, 1=Yes,2=Yes+FONT		INFO=2 shows character set at top of screen
+INFO      = 1   ; Add project info to code area 0=NO, 1=Yes,2=Yes+FONT		INFO=2 shows character set at top of screen
 BUGFIX    = 0   ; Correct Known bugs		0=No, 1=Yes			
 ;
 DEBUG 	  = 0	; Add debugging			0=No, 1=Yes
