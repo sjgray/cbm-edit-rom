7dff157a3b1464968dbb3babe7c05b4abb2e0f03
Steve Gray <sjgray@rogers.com>
2019-04-16 12:16:47 -0400

    Fix for SS40 euro crtc settings
    Oops ;-)

--- crtc-reg-ss40.asm
+++ crtc-reg-ss40.asm
@@ -1,8 +1,8 @@
 ; PET/CBM EDIT ROM - CRTC Register Tables
 ; ================
-; For "Soft40" configurations. Only for 80-column machines.
+; For "Software Switchable Soft40" configurations. Only for 80-column machines.
 ; Sets the CRTC Table depending on options:
-; COLUMNS ---- 40 or 80 column hardware (with special cases 90 and 32)
+; Uses REFRESH setting to select proper config
 ;
 ;======================================
 ; NORMAL 4000/8000/8296/SuperPET MODELS
@@ -11,8 +11,8 @@
 
 ;---------------------- Soft 40x25, 50 Hz, 20 kHz for 12" Internal Monitor (inverted video)
 !IF REFRESH=0 {
-CRT_CONFIG_TEXT2:	!byte $31,$14,$1f,$0f,$28,$05,$19,$21,$00,$09,$00,$00,$10,$00,$00,$00,$00,$00 
-CRT_CONFIG_GRAPHICS2: 	!byte $31,$14,$1f,$0f,$28,$05,$19,$21,$00,$07,$00,$00,$10,$00,$00,$00,$00,$00
+CRT_CONFIG_TEXT2:	!byte $31,$14,$1F,$0F,$31,$00,$19,$25,$00,$09,$00,$00,$10,$00,$00,$00,$00,$00 
+CRT_CONFIG_GRAPHICS2: 	!byte $31,$14,$1F,$0F,$31,$00,$19,$25,$00,$07,$00,$00,$10,$00,$00,$00,$00,$00
 }
 
 ;---------------------- Soft 40x25, 60 Hz, 20 kHz for 12" Internal Monitor (inverted video)
@@ -50,12 +50,12 @@ CRT_CONFIG_GRAPHICS2:	!byte $31,$14,$1f,$0f,$28,$05,$19,$21,$00,$07,$00,$00,$10,
 ;===============
 ; The 8296D has a different monitor than standard machines and requires slightly modified timing
 
-;---------------------- 40/80x25, 50 Hz, 16.949 kHz for 12" Internal 8296D Monitor (V1)
+;---------------------- 40x25, 50 Hz, 16.949 kHz for 12" Internal 8296D Monitor (V1)
 !IF REFRESH=82 {
 ; **** NEEDS FIXED ****
 }
 
-;---------------------- 40/80x25, 50 Hz, 16.949 kHz for 12" Internal 8296D Monitor (V2)
+;---------------------- 40x25, 50 Hz, 16.949 kHz for 12" Internal 8296D Monitor (V2)
 !IF REFRESH=83 {
 ; **** NEEDS FIXED ****
 }
@@ -68,19 +68,19 @@ CRT_CONFIG_GRAPHICS2:	!byte $31,$14,$1f,$0f,$28,$05,$19,$21,$00,$07,$00,$00,$10,
 ;       These settings push the monitor to it's maximum possible resolution. Because of this you loose
 ;       the ability to expand the line height in 'text' mode.
 
-;---------------------- 40/80x32, 60 Hz, ?? kHz for 12" Internal Monitor (8296 ONLY!!!!)
+;---------------------- 40x32, 60 Hz, ?? kHz for 12" Internal Monitor (8296 ONLY!!!!)
 ; **** NEEDS FIXED ****
 !IF REFRESH=90 {
 
 }
 
-;---------------------- 40/80x35, 60 Hz, ?? kHz for 12" Internal Monitor (8296 ONLY!!!!)
+;---------------------- 40x35, 60 Hz, ?? kHz for 12" Internal Monitor (8296 ONLY!!!!)
 ; **** NEEDS FIXED ****
 !IF REFRESH=91 {
 
 }
 
-;---------------------- 45/90x35, 60 Hz, ?? kHz for 12" Internal Monitor (8296 ONLY!!!!)
+;---------------------- 45x35, 60 Hz, ?? kHz for 12" Internal Monitor (8296 ONLY!!!!)
 ; **** NEEDS FIXED ****
 !IF REFRESH=92 {
 
